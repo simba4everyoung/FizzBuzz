32aba542fecf68ca42e1468f2fd6d131b1007436
xkun.xu@gmail.com <simba>
2014-12-01 14:54:37 -0500

    FizzBuzz Refactor
    Use function.

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 $(document).ready(function() {
-	var count;
-	var num;
 
-	for (count = 1 ; count <= 100 ; count++) {
+	$('button').on('click',function() {
+		var input = $('#input').val();
+		
+		var output = fizzBuzz(input);
 
-		if (count % 3 === 0 && count % 5 === 0) {
-			num="Fizz Buzz";	
-		}
-		else if (count % 3 === 0) {
-			num="Fizz";
-		}
-		else if (count % 5 === 0) {
-			num="Buzz";
-		}
-		else {
-			num=count;
-		}
-		var listNum="<p>" + num + "</p>";
+		console.log(output);
+		$('body').append("<p>"+output+"<p>");
 
-		$('body').append(listNum);
-		console.log(num);
+
+	})
+
+
+	var fizzBuzz = function(count) {
+		count = +count;		/*Transfer the type from string to number*/
+		var num;
+
+			if (count % 1 != 0 || count < 1 || count > 100) {		/*Examine if the input is an integer between 1 and 100*/
+				num="Please input an integer between 1 and 100";
+			}
+			else if (count % 3 === 0 && count % 5 === 0) {
+				num="Fizz Buzz";	
+			}
+			else if (count % 3 === 0) {
+				num="Fizz";
+			}
+			else if (count % 5 === 0) {
+				num="Buzz";
+			}
+			else {
+				num=count;
+			}
+			/*var listNum="<p>" + num + "</p>";*/
+
+			return num;
+		
 	}
+
 })
 
 
